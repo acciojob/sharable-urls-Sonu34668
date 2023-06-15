e9f70c2fc0c7eb24d09080cec70ee805cf5d3cad
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-15 23:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
- document.getElementById("button").addEventListener("click", function() {
-      var name = document.getElementById("name").value;
-      var year = document.getElementById("year").value;
-      var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + year;
-      document.getElementById("url").textContent = url;
-    });
+ // document.getElementById("button").addEventListener("click", function() {
+ //      var name = document.getElementById("name").value;
+ //      var year = document.getElementById("year").value;
+ //      var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + year;
+ //      document.getElementById("url").textContent = url;
+ //    });
